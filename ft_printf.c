0cce538425d14b0563f05958bb8dd294ebd93237
Julen Leon Larrondo <jleon-la@c3r6s3.42urduliz.com>
2024-01-25 12:43:51 +0100

    A little bit improved (:

--- ft_printf.c
+++ ft_printf.c
@@ -1,48 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 11:56:45 by jleon-la          #+#    #+#             */
+/*   Updated: 2024/01/25 12:43:25 by jleon-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
+
+int	option(char o, va_list args)
+{
+	char	c;
+
+	if (o == 'c')
+	{
+		c = (char) va_arg(args, int);
+		write(1, &c, 1);
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_printf(char const *format, ...)
+{
+	va_list	args;
+	int		total;
+	int		i;
+
+	total = 0;
+	i = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+			total += option((char) &format[++i], args);
+		write(1, &format[i], 1);
+		i++;
+	}
+	total += i;
+	va_end(args);
+	return (total);
+}
+
+int	main(void)
+{
+	ft_printf("Dragon ball %c", 'z');
+	return (0);
+}
 
 // void ft_printf(int i, ...)
 // {
 //     va_list argptr; // esta linea declara la lista de parametros
-//     va_start(argptr, i); // esta linea empieza el bucle sobre la lista de parametros (sin saber el numero de parametros)
-
+//     va_start(argptr, i); // esta linea empieza el bucle 
+//sobre la lista de parametros (sin saber el numero de parametros)
 //    if ( i == 0 ) {
-//       int n = va_arg( argptr, int ); // esto es para recuperar el valor de cada parametro segun su tipo
+//       int n = va_arg( argptr, int ); // esto es para recuperar el 
+//valor de cada parametro segun su tipo
 //       printf( "%d\n", n );
 //    } else {
 //       char *s = va_arg( argptr, char* );
 //       printf( "%s\n", s);
 //    }
 // }
-
-
 // int main(void)
 // {
 //     ft_printf("%d",42);
 //     printf("%s", "\n");
 //     printf("%d",42);
 // }
-
-int ft_printf(char const *format, ...) {
-    va_list args;
-    int total;
-    int i;
-
-    total = 0;
-    i = 0;
-    va_start(args, format);
-    while (format[i] != '\0')
-    {
-        // total += va_arg(args, int);
-
-        write(1, &format[i], 1);
-        i++;
-    }
-    va_end(args);
-    return (0);
-}
-
-int main() {
-    ft_printf("cdf", "fff");
-    // printf("c");
-    return (0);
-}
+		// total += va_arg(args, int);
--- ft_printf.h
+++ ft_printf.h
@@ -1,3 +1,23 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 16:56:01 by jleon-la          #+#    #+#             */
+/*   Updated: 2024/01/25 12:29:40 by jleon-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int	ft_printf(char const *format, ...);
+int	option(char o, va_list args);
+
+#endif
